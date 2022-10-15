4d2a56fdfc9e21e5bf5547b85208a5bd09dd4998
Miha <mishanosenkov2004@gmail.com>
2022-10-15 21:21:26 +0300

    feat cmake

--- 02_compiler/hello.cpp
+++ 02_compiler/hello.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	cout << "Hello World";
+#include "func.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	print();
 	return 0;
 }

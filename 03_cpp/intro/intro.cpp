0ab7dce3fdf9055e6a7f7eec939f3db4a772573b
Miha <mishanosenkov2004@gmail.com>
2022-10-26 01:10:20 +0300

    feat intro

---
+++ 03_cpp/intro/intro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[20];
+    cout<<"Enter your name, please" << endl;
+    cin >> name;
+    cout << endl << "Hello, " << name;
+    return 0;
+}
+
